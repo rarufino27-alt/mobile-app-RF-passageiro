c272d6c03ba1eaa37a66173424d50742514a4c5e
rarufino27-alt <rarufino27@gmail.com>
2026-02-23 14:40:49 -0300

    Atualizar o session.js

--- core/session.js
+++ core/session.js
@@ -1,11 +1,12 @@
 function checkSession(){
   const session = JSON.parse(localStorage.getItem("rf_session"));
+  
   if(!session || !session.logado){
-    window.location.href = "index.html";
+    window.location.href = "/mobile-app-RF/index.html";
   }
 }
 
 function logout(){
   localStorage.removeItem("rf_session");
-  window.location.href = "index.html";
+  window.location.href = "/mobile-app-RF/index.html";
 }
