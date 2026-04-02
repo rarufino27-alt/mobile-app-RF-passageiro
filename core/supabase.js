d4df86266e8207e3d2a56963587c6fa8387f2fdd
rarufino27-alt <rarufino27@gmail.com>
2026-04-02 08:58:31 -0300

    Update supabase.js

--- core/supabase.js
+++ core/supabase.js
@@ -1,12 +1,16 @@
 // core/supabase.js
 
-const SUPABASE_URL = "https://whzqcvvclmsaskwhzbcw.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoenFjdnZjbG1zYXNrd2h6YmN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMDY2OTEsImV4cCI6MjA5MDY4MjY5MX0.f_RxH38xH0k5UdFnXMLWa66T2fTQ2QhjL3l0qy7ls0Q";
+(function(){
 
-// cria cliente global SEM declarar variável duplicada
-if (!window.supabaseClient) {
-  window.supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-  );
-}
+  const SUPABASE_URL = "https://whzqcvvclmsaskwhzbcw.supabase.co";
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoenFjdnZjbG1zYXNrd2h6YmN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMDY2OTEsImV4cCI6MjA5MDY4MjY5MX0.f_RxH38xH0k5UdFnXMLWa66T2fTQ2QhjL3l0qy7ls0Q";
+
+  // 🔥 cria apenas uma vez e evita conflito global
+  if (!window.supabaseClient) {
+    window.supabaseClient = window.supabase.createClient(
+      SUPABASE_URL,
+      SUPABASE_ANON_KEY
+    );
+  }
+
+})();
