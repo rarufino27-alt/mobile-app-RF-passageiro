dd5a4532d295eb70a4e508ba426a03d3769306be
rarufino27-alt <rarufino27@gmail.com>
2026-04-02 08:44:51 -0300

    Update supabase.js

--- core/supabase.js
+++ core/supabase.js
@@ -3,7 +3,13 @@
 const SUPABASE_URL = "https://whzqcvvclmsaskwhzbcw.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoenFjdnZjbG1zYXNrd2h6YmN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMDY2OTEsImV4cCI6MjA5MDY4MjY5MX0.f_RxH38xH0k5UdFnXMLWa66T2fTQ2QhjL3l0qy7ls0Q";
 
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+// 🔥 PROTEÇÃO CONTRA DUPLICAÇÃO
+if (!window.supabaseClient) {
+  window.supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+  );
+}
+
+// 👇 SEMPRE usa esse
+const supabase = window.supabaseClient;
